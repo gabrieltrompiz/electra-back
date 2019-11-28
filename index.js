43d73b2a8398cdfbb74b1b14bc1d36aa483dfe92
Gabriel Trompiz <gab.tc@icloud.com>
2019-11-28 15:32:40 -0400

    initial apollo and express config

---
+++ index.js
@@ -0,0 +1,37 @@
+const throng = require('throng');
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+const port = process.env.PORT || 5000;
+
+const app = require('express')();
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const start = () => {
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  };
+
+  app.get('/', (req, res) => {
+    res.status(200).json({
+      status: 200,
+      message: 'Ok'
+    })
+  })
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  server.applyMiddleware({ app, path: '/' })
+
+  app.listen({ port }, () => { console.log(`Listening on port ${port}`) })
+}
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, start);
